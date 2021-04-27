149626c7948299056ead58aa1197d2f9f16db2b9
Daniil Kuzmin <dazekamikadze19962002@gmail.com>
2021-04-27 13:38:29 +0300

    popups

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -41,9 +41,67 @@ class Init {
       _this.actions().autoGrowTextarea(this)
     })
 
+    const emailInput = document.querySelectorAll('input[data-type="email"]')
+    emailInput.forEach((item) => {
+      item.addEventListener('blur', function () {
+        _this.actions().checkEmail(this)
+      })
+    })
+
+    const noTelAndEmailInput = document.querySelectorAll(
+      'input:not([data-type="tel"]):not([data-type="email"]), textarea'
+    )
+    noTelAndEmailInput.forEach((item) => {
+      item.addEventListener('blur', function () {
+        _this.actions().checkOtherInputs(this)
+      })
+    })
+
     window.ap(document).on('click', '.tabs__btn', function (e) {
       e.preventDefault()
       _this.actions().tabChange(this)
+    })
+
+    window.ap(document).on('click', '.tabs__btn', function (e) {
+      e.preventDefault()
+      _this.actions().tabChange(this)
+    })
+
+    window.ap(document).on('click', '.popup-btn', function (e) {
+      e.preventDefault()
+      _this.actions().showPopup(this)
+    })
+
+    window.ap(document).on('click', '.overlay, .popup__close', function (e) {
+      e.preventDefault()
+      _this.actions().hidePopup(this)
+    })
+
+    window.ap(document).on('click', '.select-open', function (e) {
+      e.preventDefault()
+      _this.actions().toggleSelect(this)
+    })
+
+    window.ap(document).on('input', '.select-block input', function (e) {
+      _this.actions().changeSelectItem(this)
+    })
+
+    document.addEventListener('click', (e) => {
+      const selectBtn = document.querySelectorAll('.select-open')
+      const container = document.querySelector('.select-block')
+      if (selectBtn.length) {
+        selectBtn.forEach((item) => {
+          if (
+            e.target !== container &&
+            e.target.closest('.select-block') === null &&
+            e.target !== item &&
+            e.target.closest('.select-open') === null
+          ) {
+            item.classList.remove('active')
+            item.closest('.select').querySelector('.select-block').classList.remove('active')
+          }
+        })
+      }
     })
   }
 
@@ -97,12 +155,37 @@ class Init {
           })
         })
       },
+      checkEmail(el) {
+        const pattern = /^[a-z0-9_.-]+@[a-z0-9_.-]+\.([a-z]{1,6}\.)?[a-z]{2,6}$/i
+        if (el.value !== '') {
+          if (el.value.search(pattern) === 0) {
+            el.classList.remove('input-err')
+            el.classList.add('input-border')
+          } else {
+            el.classList.add('input-err')
+            el.classList.remove('input-border')
+          }
+        } else {
+          el.classList.remove('input-err')
+          el.classList.remove('input-border')
+        }
+      },
+      checkOtherInputs(el) {
+        if (el.value !== '') {
+          el.classList.add('input-border')
+        } else {
+          el.classList.remove('input-border')
+        }
+      },
       showBody() {
         document.querySelector('body').style.opacity = 1
       },
       autoGrowTextarea(el) {
         el.style.height = '2px'
         el.style.height = el.scrollHeight + 'px'
+      },
+      getScrollbarWidth() {
+        return window.innerWidth - document.documentElement.clientWidth
       },
       tabChange(el) {
         const tabDataAttr = el.getAttribute('data-tab')
@@ -126,6 +209,55 @@ class Init {
         })
 
         el.classList.add('tabs__btn--active')
+      },
+      showPopup(el) {
+        const overlay = document.querySelector('.overlay')
+        const popupSelector = el.getAttribute('data-popup')
+        const popup = document.querySelector(`.popup[data-popup="${popupSelector}"]`)
+        const popupClose = popup.querySelector('.popup__close')
+        const popupServices = el.getAttribute('data-services')
+
+        el.classList.add('popup-trigger')
+        popup.classList.add('popup--active')
+        overlay.classList.add('overlay--active')
+        if (this.getScrollbarWidth()) {
+          document.querySelector('html').classList.add('compensate-for-scrollbar')
+        }
+        document.querySelector('html').classList.add('fixed')
+        if (document.documentElement.clientWidth > 1024) {
+          popupClose.focus()
+        }
+
+        if (popupServices) {
+          popup.querySelector('.select-open span').innerText = popupServices
+          popup.querySelector(`.select-block input[value="${popupServices}"]`).checked = true
+        }
+      },
+      hidePopup(el) {
+        if (el.classList.contains('overlay--menu')) {
+          el.classList.remove('overlay--menu')
+          document.querySelector('.header__nav').classList.remove('header__nav--active')
+          document.querySelector('.header__burger').classList.remove('header__burger--active')
+        } else {
+          document.querySelector('html').classList.remove('compensate-for-scrollbar')
+          document.querySelector('html').classList.remove('fixed')
+          const overlay = document.querySelector('.overlay')
+          const popup = document.querySelector('.popup--active')
+          const popupTrigger = document.querySelector('.popup-trigger')
+          popup.classList.remove('popup--active')
+          overlay.classList.remove('overlay--active')
+          popupTrigger.focus()
+          popupTrigger.classList.remove('popup-trigger')
+        }
+      },
+      toggleSelect(el) {
+        el.classList.toggle('active')
+        el.closest('.select').querySelector('.select-block').classList.toggle('active')
+      },
+      changeSelectItem(el) {
+        const selectOpen = el.closest('.select').querySelector('.select-open')
+        _this.actions().toggleSelect(selectOpen)
+        el.closest('.select').querySelector('.select-open span').innerText = el.value
       }
     }
   }
